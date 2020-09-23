408f83ec878e9ae9aeea19bb2021c72f5c2cbd1c
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-09-23 05:09:42 +0300

    Fix filter. Refactor Filter.

--- srv/resources/assets/script/components/CaseHistory/Filter/index.tsx
+++ srv/resources/assets/script/components/CaseHistory/Filter/index.tsx
@@ -3,74 +3,36 @@ import cn from 'classnames'
 
 type F<T> = FunctionalComponent<T>
 
-type Props = {
-  onChange: (type: string) => void
+type ForButtons = {
+  buttons: BtnSpec[]
+  activeButtons: Set<string>
+  onChange(key: string): void
 }
 
-export type Button = {
-  type: string
-  alt: string
+type Props = ForButtons
+
+export type BtnSpec = {
+  key: string
+  name: string
   icon: string
-  show: boolean
-  onChange: (type: string) => void
 }
 
-export const Filter: F<Props> = ({onChange}) => {
-  const buttons: Button[] = [
-    {type: 'action', alt: 'Показать действия', icon: 'briefcase', show: false, onChange},
-    {type: 'comment', alt: 'Показать комментарии', icon: 'bullhorn', show: false, onChange},
-    {type: 'partnerCancel', alt: 'Показать отказы партнёров', icon: 'remove-circle', show: false, onChange},
-    {type: 'partnerDelay', alt: 'Показать опоздания партнёров', icon: 'time', show: false, onChange},
-    {type: 'call', alt: 'Показать звонки', icon: 'phone-alt', show: false, onChange},
-    {type: 'smsForPartner', alt: 'Показать SMS партнёрам', icon: 'envelope', show: false, onChange},
-    {type: 'eraGlonassIncomingCallCard', alt: 'Показать поступления «Карточек Вызова» ЭРА-ГЛОНАСС', icon: 'globe', show: false, onChange},
-    {type: 'locationSharingResponse', alt: 'Показать запросы на определение координат', icon: 'map-marker', show: false, onChange},
-    {type: 'customerFeedback', alt: 'Показать отзывы клиентов', icon: 'star', show: false, onChange},
-  ]
+export const Filter: F<Props> = ({onChange, activeButtons, buttons}) => {
   return (
     <div class='btn-group btn-group-sm' role='group'>
-      <Buttons buttons={buttons}/>
+      <Buttons buttons={buttons} activeButtons={activeButtons} onChange={onChange}/>
     </div>
   )
 }
 
-type PropsButtons = {
-  buttons: Button[]
-}
-const Buttons: F<PropsButtons> = ({buttons}) => {
-  return (
-    <div>{buttons.map(({alt, icon, onChange, show, type}) => <ButtonIcon alt={alt} icon={icon} onChange={onChange} show={show} type={type}/>)}</div>
-  )
-}
-
-type buttonIcon = {
-  type: string
-  alt: string
-  icon: string
-  show: boolean
-  onChange: (type: string) => void
-}
-
-const ButtonIcon: F<buttonIcon> = ({type, alt, icon, show, onChange}) => {
-  return (
-    <div class={cn('btn btn-default', {active: show})} alt={alt} onClick={(e) => {
-      onChange(type)
-    }}>
-      <i class={`glyphicon glyphicon-${icon}`}/>
+const Buttons: F<ForButtons> = ({buttons, activeButtons, onChange}) => {
+  const btn = (b) =>
+    <div class={cn('btn btn-default', {active: activeButtons.has(b.key)})} title={b.name} onClick={() => onChange(b.key)}>
+      <i class={`glyphicon glyphicon-${b.icon}`}/>
     </div>
-  )
-}
-// .btn.btn-default(
-//   title="Показать действия",
-//     data-bind=`css: {active: histShowActi}, click: histToggleActi`)
-// i.glyphicon.glyphicon-briefcase
 
-type input = {}
-
-const Input: F<input>  = () => {
   return (
-    <div>
-
-    </div>
-  )
+    <div class='btn-group btn-group-sm' role='group'>
+      {buttons.map(btn)}
+    </div>)
 }
--- srv/resources/assets/script/components/CaseHistory/index.tsx
+++ srv/resources/assets/script/components/CaseHistory/index.tsx
@@ -1,6 +1,6 @@
 import {h, FunctionalComponent} from 'preact'
-import {Filter} from './Filter'
-import {useEffect, useMemo, useState} from 'preact/hooks'
+import {useState} from 'preact/hooks'
+import {Filter, BtnSpec} from './Filter'
 import {HistoryItem} from './HistoryItem'
 import * as Type from './types'
 
@@ -10,35 +10,78 @@ type Props = {
   caseHistory: () => Type.HistoryItem[]
 }
 
+const filterSpec: BtnSpec[] = [
+  {
+    key: 'action',
+    name: 'Показать действия',
+    icon: 'briefcase'
+  },
+  {
+    key: 'comment',
+    name: 'Показать комментарии',
+    icon: 'bullhorn'
+  },
+  {
+    key: 'partnerCancel',
+    name: 'Показать отказы партнёров',
+    icon: 'remove-circle'
+  },
+  {
+    key: 'partnerDelay',
+    name: 'Показать опоздания партнёров',
+    icon: 'time'
+  },
+  {
+    key: 'call',
+    name: 'Показать звонки',
+    icon: 'phone-alt'
+  },
+  {
+    key: 'smsForPartner',
+    name: 'Показать SMS партнёрам',
+    icon: 'envelope'
+  },
+  {
+    key: 'eraGlonassIncomingCallCard',
+    name: 'Показать поступления «Карточек Вызова» ЭРА-ГЛОНАСС',
+    icon: 'globe'
+  },
+  {
+    key: 'locationSharing',
+    name: 'Показать запросы на определение координат',
+    icon: 'map-marker'
+  },
+  {
+    key: 'customerFeedback',
+    name: 'Показать отзывы клиентов',
+    icon: 'star'
+  }
+]
+
 export const CaseHistory: F<Props> = ({caseHistory}) => {
-  const [type, setType] = useState<string>('')
-  const typeFilter = new Set(['comment'])
-
-  const typeAdd = (type: string) => {
-    typeFilter.add(type)
-    setType('')
+  const [typeFilter, setTypeFilter] = useState<Set<string>>(
+    new Set(filterSpec.map(f => f.key))
+  )
+  const toggleFilter = (key: string) => {
+    typeFilter.has(key) ? typeFilter.delete(key) : typeFilter.add(key)
+    setTypeFilter(new Set(typeFilter))
   }
-  const typeDelete = (type: string) => {
-    typeFilter.delete(type)
-    setType('')
-  }
-  useEffect(() => {
-    !!type && typeFilter.has(type) ? typeDelete(type) : typeAdd(type)
-  }, [type])
 
-  const filteredCaseHistory = caseHistory() && caseHistory().filter(x => typeFilter.has(x[2].type))
-  
+  const filteredCaseHistory = caseHistory()
+    .filter(([_time, _user, {type}]) =>
+      (type === 'avayaEvent' && typeFilter.has('call'))
+      || (type.startsWith('locationSharing') && typeFilter.has('locationSharing'))
+      || typeFilter.has(type))
+
   return (
-    <section>
-      <h4 style='float: left'> История по кейсу</h4>
+    <div id='case-history'>
+      <h4 style='float: left'>История по кейсу</h4>
       <div style='float: right'>
-        <Filter onChange={setType}/>
+        <Filter buttons={filterSpec} activeButtons={typeFilter} onChange={toggleFilter}/>
       </div>
-      <div id='case-history'>
-        <div className='well history-item'>
-          {filteredCaseHistory.map(data => <Case caseData={data}/>)}
-        </div>
-      </div>
-    </section>
+      <div style="clear:both"/>
+      {filteredCaseHistory.map(i => <HistoryItem historyData={i}/>)}
+      <a class='more' href='#'>Ещё</a>
+    </div>
   )
 }
